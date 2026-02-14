a1a784fa7bc33922640e887fdd88dddacf18e82a
Kajal Sanwal <kajalsanwal777@gmail.com>
2026-02-14 14:45:23 +0530

    mongodb plugin added

---
+++ plugins/mongodb.js
@@ -0,0 +1,15 @@
+const { default: fastify } = require("fastify");
+const fp = require("fastify-plugin");
+const mongoose = require("mongoose");
+
+module.exports = fp(async (fastify, opts) => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI);
+        fastify.decorate("mongoose", mongoose); //decorate is used for travel
+        fastify.log.info("❤️ MongoDB connected!");
+
+    } catch (err) {
+       fastify.log.error(err)
+       process.exit(1) 
+    }
+})
