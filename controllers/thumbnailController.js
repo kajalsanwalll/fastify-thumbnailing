484e3e4aa577dd9e7d01e1a166cd34b214535fbb
Kajal Sanwal <kajalsanwal777@gmail.com>
2026-02-15 16:24:12 +0530

    thumbnail controller added

--- controllers/thumbnailController.js
+++ controllers/thumbnailController.js
@@ -4,3 +4,49 @@ const fs = require("fs");
 const {pipeline} = require("stream");
 const util = require("util");
 const pipelineAsync = util.promisify(pipeline)
+
+exports.createThumbnail = async (request, reply) => {
+
+    try {
+        
+        const parts = request.part();
+
+        let fields = {};
+        let filename;
+
+        for await(const part of parts){
+
+            if(part.file){
+
+                const filename =`${Date.now()}-${part.filename}`
+                const saveTo = path.join(
+                    __dirname,
+                    "..",
+                    "uploads",
+                    "thumbnails",
+                    filename
+                );
+
+                await pipelineAsync(part.file, fs.createWriteStream(saveTo))
+
+            } else {
+                fields[part.filename] = part.value
+            }
+        }
+
+        const thumbnail = new Thumbnail({
+            user: request.user.id,
+            videoName: fields.videoName,
+            version: fields.version,
+            image: `/uploads/thumbnails/${filename}`,
+            paid: fields.paid === "true"
+        })
+
+        await thumbnail.save()
+        reply.code(201).send(thumbnail)
+
+    } catch (err) {
+        reply.send(err)
+    }
+
+}
--- server.js
+++ server.js
@@ -8,6 +8,10 @@ const fastify = require("fastify")({logger:true})
 fastify.register(require("@fastify/cors"))
 fastify.register(require("@fastify/sensible"))
 fastify.register(require("@fastify/multipart"))
+fastify.register(require("@fastify/static", {
+    root: path.join(__dirname, "uploads"),
+    prefix: "/uploads/",
+}))
 fastify.register(require("@fastify/env"),{
     dotenv: true,
     schema:{
