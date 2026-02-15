edae88c25beb89a5593a29cb9df99303c9eaaca5
Kajal Sanwal <kajalsanwal777@gmail.com>
2026-02-15 16:04:24 +0530

    registered multipart from fastify

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const fastify = require("fastify")({logger:true})
 //register plugins
 fastify.register(require("@fastify/cors"))
 fastify.register(require("@fastify/sensible"))
+fastify.register(require("@fastify/multipart"))
 fastify.register(require("@fastify/env"),{
     dotenv: true,
     schema:{
