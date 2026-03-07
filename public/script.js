5815caf4b8afa614bd565cc85def9cef22499de1
Kajal Sanwal <kajalsanwal777@gmail.com>
2026-03-07 14:04:27 +0530

    fixed route for login/signup

--- public/script.js
+++ public/script.js
@@ -1,18 +1,24 @@
-async function uploadImage(){
+async function uploadImage() {
 
 const fileInput = document.getElementById("imageInput")
 
-if(!fileInput.files.length){
+if (!fileInput.files.length) {
 alert("Select an image first")
 return
 }
 
+// detect environment
+const API =
+location.hostname === "localhost"
+? "http://localhost:3000"
+: ""
+
 const formData = new FormData()
 formData.append("image", fileInput.files[0])
 
-const response = await fetch("http://localhost:3000/upload",{
-method:"POST",
-body:formData
+const response = await fetch(`${API}/upload`, {
+method: "POST",
+body: formData
 })
 
 const data = await response.json()
@@ -21,6 +27,6 @@ const resultDiv = document.getElementById("result")
 
 resultDiv.innerHTML = `
 <h3>Thumbnail</h3>
-<img src="http://localhost:3000/${data.thumbnail}" />
+<img src="${API}/${data.thumbnail}" />
 `
 }
