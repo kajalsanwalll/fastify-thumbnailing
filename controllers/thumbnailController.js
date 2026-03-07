508c9e92da5b7cff5744b150b346b4b551bc8989
Kajal Sanwal <kajalsanwal777@gmail.com>
2026-03-07 13:56:13 +0530

    fixed upload

--- controllers/thumbnailController.js
+++ controllers/thumbnailController.js
@@ -2,8 +2,12 @@ const Thumbnail = require("../models/thumbnail.js");
 const cloudinary = require("cloudinary").v2;
 const streamifier = require("streamifier");
 
-// Cloudinary reads these env vars automatically:
-// CLOUDINARY_CLOUD_NAME, CLOUDINARY_API_KEY, CLOUDINARY_API_SECRET
+// Configure Cloudinary using environment variables
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
 
 // Helper: upload a buffer to Cloudinary via stream
 function uploadToCloudinary(buffer) {
@@ -29,14 +33,19 @@ exports.createThumbnail = async (request, reply) => {
 
         for await (const part of parts) {
             if (part.file) {
-                // Buffer the stream, then upload to Cloudinary
+
                 const chunks = [];
-                for await (const chunk of part.file) chunks.push(chunk);
+                for await (const chunk of part.file) {
+                    chunks.push(chunk);
+                }
+
                 const buffer = Buffer.concat(chunks);
 
                 const result = await uploadToCloudinary(buffer);
-                imageUrl = result.secure_url;  // permanent https URL
-                publicId = result.public_id;   // needed to delete later
+
+                imageUrl = result.secure_url;
+                publicId = result.public_id;
+
             } else {
                 fields[part.fieldname] = part.value;
             }
@@ -56,24 +65,31 @@ exports.createThumbnail = async (request, reply) => {
         });
 
         await thumbnail.save();
+
         reply.code(201).send(thumbnail);
 
     } catch (err) {
-        reply.send(err);
+        request.log.error(err);
+        reply.status(500).send({ error: "Thumbnail upload failed" });
     }
 };
 
 exports.getThumbnails = async (request, reply) => {
     try {
+
         const thumbnails = await Thumbnail.find({ user: request.user.id });
+
         reply.send(thumbnails);
+
     } catch (err) {
-        reply.send(err);
+        request.log.error(err);
+        reply.status(500).send(err);
     }
 };
 
 exports.getThumbnail = async (request, reply) => {
     try {
+
         const thumbnail = await Thumbnail.findOne({
             _id: request.params.id,
             user: request.user.id
@@ -84,14 +100,18 @@ exports.getThumbnail = async (request, reply) => {
         }
 
         reply.send(thumbnail);
+
     } catch (err) {
-        reply.send(err);
+        request.log.error(err);
+        reply.status(500).send(err);
     }
 };
 
 exports.updateThumbnail = async (request, reply) => {
     try {
+
         const updatedData = request.body;
+
         const thumbnail = await Thumbnail.findOneAndUpdate(
             { _id: request.params.id, user: request.user.id },
             updatedData,
@@ -103,13 +123,16 @@ exports.updateThumbnail = async (request, reply) => {
         }
 
         reply.send(thumbnail);
+
     } catch (err) {
-        reply.send(err);
+        request.log.error(err);
+        reply.status(500).send(err);
     }
 };
 
 exports.deleteThumbnail = async (request, reply) => {
     try {
+
         const thumbnail = await Thumbnail.findOneAndDelete({
             _id: request.params.id,
             user: request.user.id
@@ -119,24 +142,25 @@ exports.deleteThumbnail = async (request, reply) => {
             return reply.notFound("thumbnail not found!");
         }
 
-        // Delete from Cloudinary too
         if (thumbnail.publicId) {
             await cloudinary.uploader.destroy(thumbnail.publicId);
         }
 
         reply.send({ message: "thumbnail deleted!" });
+
     } catch (err) {
-        reply.send(err);
+        request.log.error(err);
+        reply.status(500).send(err);
     }
 };
 
 exports.deleteAllThumbnails = async (request, reply) => {
     try {
+
         const thumbnails = await Thumbnail.find({ user: request.user.id });
 
         await Thumbnail.deleteMany({ user: request.user.id });
 
-        // Delete all from Cloudinary in parallel
         await Promise.all(
             thumbnails
                 .filter(t => t.publicId)
@@ -144,7 +168,9 @@ exports.deleteAllThumbnails = async (request, reply) => {
         );
 
         reply.send({ message: "All thumbnails deleted!" });
+
     } catch (err) {
-        reply.send(err);
+        request.log.error(err);
+        reply.status(500).send(err);
     }
 };
